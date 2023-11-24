d1777e645d7c17e5bcea1f26565b8812768c5011
Honbard <145313410+Honbard@users.noreply.github.com>
2023-11-25 01:45:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function login() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const errorMessage = document.getElementById('error-message');
-
-  const account = accounts.find(
-    (acc) => acc.username === username && acc.password === password
-  );
-
   if (account) {
     document.querySelector('.login-page').style.display = 'none';
     document.querySelector('.main-page').style.display = 'block';
